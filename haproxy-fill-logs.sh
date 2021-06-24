188add546f8b0f8a602154566077ba48643a0834
Tom Dean <57017374+tom-dean-ACG@users.noreply.github.com>
2021-06-24 17:55:08 -0500

    Update haproxy-fill-logs.sh

--- haproxy-fill-logs.sh
+++ haproxy-fill-logs.sh
@@ -5,16 +5,16 @@
 # 6/24/2021 - Tom Dean
 
 # Run ApacheBench tests
-ab -n 1000 -c 10 https://www.site1.com/ > ~/ab_site1.log &
-ab -n 1000 -c 10 https://www.site2.com/ > ~/ab_site2.log &
+ab -n 1000 -c 10 https://www.site1.com/ > ~/ab_site1.log > /dev/null 2>&1 &
+ab -n 1000 -c 10 https://www.site2.com/ > ~/ab_site2.log > /dev/null 2>&1 &
 
 # Run curl tests
-for conn in `seq 1 1000` ; do curl -k https://www.site1.com/ ; done &
-for conn in `seq 1 1000` ; do curl -k https://www.site2.com/ ; done &
+for conn in `seq 1 1000` ; do curl -k https://www.site1.com/ ; done > /dev/null 2>&1 &
+for conn in `seq 1 1000` ; do curl -k https://www.site2.com/ ; done > /dev/null 2>&1 &
 
 # Run wget tests
-for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site1.com/test.txt ; done &
-for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site2.com/test.txt ; done &
+for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site1.com/test.txt ; done > /dev/null 2>&1 &
+for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site2.com/test.txt ; done > /dev/null 2>&1 &
 
 # Run SSH test
-for conn in `seq 1 100` ; do bash -c 'scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 2222 cloud_user@ssh.site3.com:/sshfiles/ssh-test.txt . &' ; done &
+for conn in `seq 1 100` ; do bash -c 'scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 2222 cloud_user@ssh.site3.com:/sshfiles/ssh-test.txt . > /dev/null 2>&1 &' ; done &
