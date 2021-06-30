e4642b2d23d7ffd0080dd5c396c76ece5660f190
Tom Dean <57017374+tom-dean-ACG@users.noreply.github.com>
2021-06-30 13:22:25 -0500

    Update haproxy-fill-logs.sh

--- haproxy-fill-logs.sh
+++ haproxy-fill-logs.sh
@@ -5,14 +5,20 @@
 # 6/30/2021 - Tom Dean
 
 # Run ApacheBench tests
-ab -n 1000 -c 10 https://www.site1.com/test.txt > ~/ab_site1.log > /dev/null 2>&1 &
-ab -n 1000 -c 10 https://www.site2.com/test.txt > ~/ab_site2.log > /dev/null 2>&1 &
+ab -n 1000 -c 10 https://www.site1.com/ > ~/ab_site1.log > /dev/null 2>&1 &
+ab -n 1000 -c 10 https://www.site2.com/ > ~/ab_site2.log > /dev/null 2>&1 &
+ab -n 1000 -c 10 https://www.site1.com/test.txt > ~/ab_site1_test.log > /dev/null 2>&1 &
+ab -n 1000 -c 10 https://www.site2.com/test.txt > ~/ab_site2_test.log > /dev/null 2>&1 &
 
 # Run curl tests
+for conn in `seq 1 1000` ; do curl -k https://www.site1.com/ ; done > /dev/null 2>&1 &
+for conn in `seq 1 1000` ; do curl -k https://www.site2.com/ ; done > /dev/null 2>&1 &
 for conn in `seq 1 1000` ; do curl -k https://www.site1.com/test.txt ; done > /dev/null 2>&1 &
 for conn in `seq 1 1000` ; do curl -k https://www.site2.com/test.txt ; done > /dev/null 2>&1 &
 
 # Run wget tests
+for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site1.com/ ; done > /dev/null 2>&1 &
+for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site2.com/ ; done > /dev/null 2>&1 &
 for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site1.com/test.txt ; done > /dev/null 2>&1 &
 for conn in `seq 1 1000` ; do wget --no-check-certificate -O - https://www.site2.com/test.txt ; done > /dev/null 2>&1 &
 
